941866eb00df1cfe100fe8fb695647493706eeef
rakibsarowar <rakibsarowar@gmail.com>
2023-11-18 21:19:29 +0600

    added bootstrap and include form

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import TodoApp from './components/TodoApp.vue'
+</script>
+
+<template>
+  <div>
+
+  </div>
+  <TodoApp msg="Vite + Vue" />
+</template>
+
+<style scoped></style>
---
+++ src/components/TodoApp.vue
@@ -0,0 +1,17 @@
+<script setup>
+</script>
+
+<template>
+<div class="container">
+<h2 class="text-center mt-5"> My Vue Todo App</h2>
+
+<!-- imput -->
+<div class="d-flex">
+  <input type="text" placeholder="Enter task" class="form-control">
+  <button class="btn btn-warning rounded-0"> Submit</button>
+</div>
+</div>
+</template>
+
+<style scoped>
+</style>
